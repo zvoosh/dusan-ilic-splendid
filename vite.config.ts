af105783afda5b188c46ef7bda33d7999466dbfe
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-11-20 22:40:12 +0100

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/zvoosh.github.io/",
   resolve: {
     alias: {
       '@components': './src/components', // Corrected alias path
