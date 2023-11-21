7126eeab58f22cce7738b40e305130275151bb70
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-11-21 14:21:52 +0100

    add: deploy workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+// import ReactDOM from 'react-dom/client'
+// // import App from './App.tsx'
+// import { BrowserRouter } from "react-router-dom";
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <BrowserRouter >
+//     {/* <App /> */}
+//     <div>
+//       dusan dadadad
+//     </div>
+//   </BrowserRouter>
+// )
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter >
-    {/* <App /> */}
-    <div>
-      dusan dadadad
-    </div>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// add the beginning of your app entry
-// import 'vite/modulepreload-polyfill'
 
 export default defineConfig({
   plugins: [react()],
   base: "/dusan-ilic-splendid/",
-  // resolve: {
-  //   alias: {
-  //     '@components': './src/components', // Corrected alias path
-  //   },
-  // },
-  // build: {
-  //   // generate .vite/manifest.json in outDir
-  //   manifest: true,
-  //   rollupOptions: {
-  //     // overwrite default .html entry
-  //     input: 'src/main.tsx',
-  //   },
-  // },
 });
