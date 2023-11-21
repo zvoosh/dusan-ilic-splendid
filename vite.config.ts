bc99d4625b4f3316ace480a3e324a9713f1de237
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-11-21 12:32:15 +0100

    fix: fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// add the beginning of your app entry
+// import 'vite/modulepreload-polyfill'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +10,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@components': './src/components', // Corrected alias path
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: 'src/main.tsx',
     },
   },
 });
