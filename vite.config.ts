d87d50880ce95fe7a80061a37bbb8689bf93d1b0
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-11-21 13:11:23 +0100

    fix: fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/zvoosh.github.io/",
+  base: "/dusan-ilic-splendid/",
   // resolve: {
   //   alias: {
   //     '@components': './src/components', // Corrected alias path
